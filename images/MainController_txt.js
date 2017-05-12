440920da1fb4b86385b2a76477fd79b8268cf937
mungs93 <mungs9@gmail.com>
2017-05-12 03:17:01 -0400

    Save

--- images/MainController_txt.js
+++ images/MainController_txt.js
@@ -3,30 +3,39 @@ app.controller('MainController', ['$scope', function($scope) {
   $scope.promo = 'Let\'s make meaningful and lasting changes around us';
   $scope.products = [
   	{ 
+  		name: 'Fort Bend Pets Alive', 
+    	price: 12,
+    	pubdate: new Date('2017', '05', '01'), 
+    	cover: 'images/FBPA.jpg',
+    	likes: 0,
+  	}, 
+  	
+  	
+  	{ 
+    	name: 'Saving Our Companion Animals', 
+    	price: 12, 
+    	pubdate: new Date('2017', '05', '01'), 
+    	cover: 'images/SOCA.png',
+    	likes: 0,
+  	}, 
+  	{ 
     	name: 'Restoration of natural habitat & creation of eco-friendly site of natural exploration along the San Marcos River', 
     	price: 12,
-    	pubdate: new Date('2017', '03', '31'), 
-    	cover: 'images/San_Marcos_River.jpg',
+    	pubdate: new Date('2017', '05', '01'), 
+    	cover: 'images/San_Marcos_mod.png',
     	likes: 0,
   	}, 
-  	{ 
-    	name: 'Preservation of natural habitat & animal species', 
-    	price: 10, 
-    	pubdate: new Date('2017', '02', '21'), 
-    	cover: 'images/butterfly.jpg',
-    	likes: 0,
-  	}, 
-  	{ 
-    	name: 'Assisting seniors with moving homes and other daily tasks', 
-    	price: 10,
-    	pubdate: new Date('2017', '04', '01'), 
-    	cover: 'images/habitat.jpg',
+  	  	{ 
+    	name: 'Fort Bend Apartment Locators service for residents of the great community', 
+    	price: 12,
+    	pubdate: new Date('2017', '05', '01'), 
+    	cover: 'images/image_1.png',
     	likes: 0,
   	}, 
   	{ 
     	name: 'Continued TextbookConsultants service for future semesters and students', 
     	price: 5,
-    	pubdate: new Date('2017', '01', '01'), 
+    	pubdate: new Date('2017', '05', '01'), 
     	cover: 'images/TB_logo.jpg',
     	likes: 0,
   	}

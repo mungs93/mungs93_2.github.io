e05b0799e056db36c7f17a83369730a4cdaf201f
mungs93 <mungs9@gmail.com>
2017-05-19 15:40:59 -0400

    Save

--- images/MainController.js
+++ images/MainController.js
@@ -10,6 +10,15 @@ app.controller('MainController', ['$scope', function($scope) {
 	    "price": "Learn More" 
 	  }, 
 	  { 
+	    "icon": 'images/SOCA.png', 
+	    "title": 'Saving Our Companion Animals', 
+	    "developer": 'SOCA – FBC works with local veterinarians to provide medical care for sick/injured animals at Fort Bend County Animal Services (FBCAS) that are not taken in by local rescue groups. SOCA – FBC gathers donations to provide supplies and medicine for the animals at FBCAS. SOCA – FBC also manages the foster program at FBCAS, holds adoption events and fund-raisers, and transports animals to out-of-state rescues for adoption.', 
+	    "url": "https://www.soca-fbc.org",
+		"url2": "https://www.facebook.com/soca.fortbendcounty/",
+		"url3": "https://www.paypal.com/donate/?token=KD42aCf0umIHLhrj4Pl17sFfvQ50hAjiz3h7bu_NTfRMOt31DusTuEwuBBbDLRK9iYXaUG",	    
+	    "price": "Learn More" 
+	  },
+	  { 
 	    "icon": 'images/peanut.png', 
 	    "title": 'A Place for Peanut', 
 	    "developer": 'A Place for Peanut was founded in January 2016 so that others can experience the joy of spending quality time with a horse. But it is also a sanctuary for horses who would otherwise face unspeakable plights.', 

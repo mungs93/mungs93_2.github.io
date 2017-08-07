6304650009e9be78af74bde546845559d4d01d5b
mungs93 <mungs9@gmail.com>
2017-08-07 19:24:11 -0400

    Save

--- mainapp/MainController.js
+++ mainapp/MainController.js
@@ -4,35 +4,30 @@ app.controller('MainController', ['$scope', function($scope) {
 	    icon: 'images/Harvard.jpg', 
 	    title: 'Harvard', 
 	    developer: 'Memorial Church, Cambridge, MA.', 
-	    price: ,
 	    link: "TC_Textbooks.html"
 	  }, 
 	  { 
 	    icon: 'images/Yale2.jpg', 
 	    title: 'Yale', 
 	    developer: 'Chico Dusty', 
-	    price: ,
 	    link: "TC_Textbooks.html"	    
 	  },
 	  {
 	    icon: 'images/Stanford2.jpg',
 	    title: 'Stanford',
 	    developer: 'Memorial Church, Stanford, CA.',
-	    price: ,
 	    link: "TC_Textbooks.html"	    
 	  },
 	  {
 	    icon: 'images/Penn2.jpg',
 	    title: 'Penn',
 	    developer: 'College Hall, Philadelphia, PA.',
-	    price: ,
 	    link: "TC_Textbooks.html"	    
 	  },
 	  {
 	    icon: 'images/All2.jpg',
 	    title: 'More Colleges',
 	    developer: 'Silverman Hall, Philadelphia, PA',
-	    price: ,
 	    link: "TC_Textbooks.html"
 	  }
 	];

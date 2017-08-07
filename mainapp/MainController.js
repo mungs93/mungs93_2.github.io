89be867d61e87d4413398b9badb9928f92843324
mungs93 <mungs9@gmail.com>
2017-08-07 19:21:19 -0400

    Save

--- mainapp/MainController.js
+++ mainapp/MainController.js
@@ -4,7 +4,6 @@ app.controller('MainController', ['$scope', function($scope) {
 	    icon: 'images/Harvard.jpg', 
 	    title: 'Harvard', 
 	    developer: 'Memorial Church, Cambridge, MA.', 
-	    developer2: '1340 classes and 1360 books', 
 	    price: ,
 	    link: "TC_Textbooks.html"
 	  }, 
@@ -12,7 +11,6 @@ app.controller('MainController', ['$scope', function($scope) {
 	    icon: 'images/Yale2.jpg', 
 	    title: 'Yale', 
 	    developer: 'Chico Dusty', 
-	    developer2: '1340 classes and 1360 books', 
 	    price: ,
 	    link: "TC_Textbooks.html"	    
 	  },
@@ -20,7 +18,6 @@ app.controller('MainController', ['$scope', function($scope) {
 	    icon: 'images/Stanford2.jpg',
 	    title: 'Stanford',
 	    developer: 'Memorial Church, Stanford, CA.',
-	    developer2: '1340 classes and 1360 books', 
 	    price: ,
 	    link: "TC_Textbooks.html"	    
 	  },
@@ -28,7 +25,6 @@ app.controller('MainController', ['$scope', function($scope) {
 	    icon: 'images/Penn2.jpg',
 	    title: 'Penn',
 	    developer: 'College Hall, Philadelphia, PA.',
-	    developer2: '1340 classes and 1360 books', 
 	    price: ,
 	    link: "TC_Textbooks.html"	    
 	  },
@@ -36,7 +32,6 @@ app.controller('MainController', ['$scope', function($scope) {
 	    icon: 'images/All2.jpg',
 	    title: 'More Colleges',
 	    developer: 'Silverman Hall, Philadelphia, PA',
-	    developer2: '1340 classes and 1360 books', 
 	    price: ,
 	    link: "TC_Textbooks.html"
 	  }
